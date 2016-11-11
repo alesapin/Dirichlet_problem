b5c3d62d1ff1a1338456d56989e0c79808ac2e6b
alesapin <alesapin@gmail.com>
2016-11-11 23:36:39 +0300

    Added options and output

--- main_par.cpp
+++ main_par.cpp
@@ -3,6 +3,11 @@
 #include "mesh.h"
 #include "iter.h"
 #include <mpi.h>
+#include <getopt.h>
+#include <cstring>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
 
 double F(PointD p) {
     double x = p.first;
@@ -39,32 +44,72 @@ int getPowOfTwo(int val){
     return pwr;
 }
 
+struct Params {
+    long a;
+    long b;
+    long rows;
+    long cols;
+    std::string fname;
+};
+
+Params getParams(int argc, char** argv) {
+      char c;
+      char *opt_val = NULL;
+      Params result;
+      while ((c = getopt (argc, argv, "a:b:r:c:f:")) != -1) {
+          switch(c) {
+            case 'a':
+                opt_val = optarg;
+                result.a = strtol(opt_val, NULL, 10);
+                break;
+            case 'b':
+                opt_val = optarg;
+                result.b = strtol(opt_val, NULL, 10);
+                break;
+            case 'r':
+                opt_val = optarg;
+                result.rows = strtol(opt_val, NULL, 10);
+                break;
+            case 'c':
+                opt_val = optarg;
+                result.cols = strtol(opt_val, NULL, 10);
+                break;
+            case 'f':
+                opt_val = optarg;
+                result.fname = std::string(opt_val);
+                break;
+          }
+      }
+      return result;
+}
+
+
 const double EPSILON = 0.0001;
 
 int main(int argc, char **argv) {
     int size, rank;
+    Params pars = getParams(argc, argv);
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    double A = 4, B = 4;
-    int totalRows = 1000, totalCols = 1000;
+    long A = pars.a, B = pars.b;
+    int totalRows = pars.cols, totalCols = pars.rows;
     long rowsShift, colsShift;
     long rows, cols;
     MPI_Status status;
-
+    double start;
     int sizePower = getPowOfTwo(size);
     PointUi ps = splitFunction(totalRows, totalCols, sizePower);
     int procRows =  1 << ps.first, procCols = 1<< ps.second;
     std::map<int, Mesh> splited;
     MPI_Request dummy;
     if (rank == 0) {
+        start = MPI_Wtime();
         Mesh result(PointUi(0,0), PointUi(A,B), totalRows, totalCols, totalRows, totalCols);
         initMeshBoundaries(result, phi);
         splited = splitMesh(result, sizePower);
-        //std::cerr << "Total size: " << splited.size() << "\n";
         for(std::map<int, Mesh>::iterator itr = splited.begin(); itr != splited.end(); ++itr) {
-            //std::cerr << itr->first << "\n";
             long rShift = itr->second.getRowsShift();
             long cShift = itr->second.getColumnsShift();
             long r = itr->second.getRows();
@@ -115,24 +160,32 @@ int main(int argc, char **argv) {
         }
         MPI_Barrier(MPI_COMM_WORLD);
     }
-//    if (rank != 0) {
-//        MPI_Send();
-//    }else{
-//        std::map<int, Mesh> submeshs;
-//        submeshs[0] = curMesh;
-//        std::vector<MPI_Request> requests;
-//        for(int i = 1; i < size; ++i ){
-//            MPI_Request rec;
-//            MPI_Recv(&rows, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-//            MPI_Recv(&cols, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-//            MPI_Recv(&rowsShift, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-//            MPI_Recv(&colsShift, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-//            double *recIdata = new double[rows*cols];
-//            MPI_Irecv(recIdata, rows*cols, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, &rec);
-//            requests.push_back(rec);
-//            Mesh curMesh(PointUi(0,0), PointUi(A,B), rows, cols, totalRows, totalCols, recdata, rowsShift, colsShift);
-//        }
-//    }
+    if (rank != 0) {
+        MPI_Send(&rows, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&cols, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&rowsShift, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&colsShift, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(curMesh.getData(), rows*cols, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }else{
+        std::map<int, Mesh> submeshs;
+        submeshs[0] = curMesh;
+        std::vector<MPI_Request> requests;
+        for (int i = 1; i < size; ++i ) {
+            MPI_Recv(&rows, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&cols, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&rowsShift, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&colsShift, 1, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            double *recIdata = new double[rows*cols];
+            MPI_Recv(recIdata, rows*cols, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            Mesh curMesh(PointUi(0,0), PointUi(A,B), rows, cols, totalRows, totalCols, recIdata, rowsShift, colsShift);
+            submeshs[i] = curMesh;
+        }
+        Mesh result = collectMesh(submeshs);
+        double elapsed = MPI_Wtime() - start;
+        std::ofstream ofs(pars.fname.c_str());
+        dropToStream(ofs, result);
+        ofs <<"stats:" << totalRows << '\t' << totalCols << '\t' << elapsed;
+    }
     MPI_Finalize();
     return 0;
 }
